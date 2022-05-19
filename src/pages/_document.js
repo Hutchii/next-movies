ab65924507f520878e9866db62ad9d07dd78f9d8
Hutchii <ravodola@gmail.com>
2022-05-19 12:44:48 +0200

    Font fix #3

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,13 +6,13 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600&display=optional"
             rel="stylesheet"
           />
-          <link rel="stylesheet" href="https://use.typekit.net/erq2dah.css" />
+          {/* <link rel="stylesheet" href="https://use.typekit.net/erq2dah.css" /> */}
         </Head>
         <body>
           <Main />
