f13ea6de3d1984bdf74f387ca8012deaf73b4d78
Hutchii <ravodola@gmail.com>
2022-05-21 12:18:54 +0200

    Mobile search test

--- src/components/Movies/MoviesSearch.js
+++ src/components/Movies/MoviesSearch.js
@@ -1,23 +1,36 @@
 import Search from "../../../public/svg/Search.svg";
 import { useRouter } from "next/router";
 import styled from "styled-components";
+import { useEffect } from "react";
 
 export default function MoviesSearch({ refHandler, onChangeHandler }) {
   const { query } = useRouter();
+
+  useEffect(() => {
+    const eventAction = (e) => {
+      if (e.code === "Enter") {
+        refHandler.current.blur();
+      }
+    };
+    refHandler.current.addEventListener("keyup", (e) => {
+      eventAction(e);
+    });
+    return refHandler.current.removeEventListener("keyup", (e) => {
+      eventAction(e);
+    });
+  }, [refHandler]);
 
   return (
     <SearchStyled>
       <InputStyled
         ref={refHandler}
-        type="text"
+        type="search"
         placeholder={`search ${
           query.genre && query.genre !== "all" ? `in #${query.genre}` : ""
         }`}
         onChange={onChangeHandler}
       />
-      <IconStyled>
-        <Search />
-      </IconStyled>
+      <Search />
     </SearchStyled>
   );
 }
@@ -27,6 +40,12 @@ const SearchStyled = styled.div`
   display: flex;
   align-items: center;
   position: relative;
+  svg {
+    position: absolute;
+    left: 0;
+    fill: var(--white);
+    margin: 0rem 0 0 0.8rem;
+  }
   @media (min-width: 480px) {
     display: inline-flex;
   }
@@ -37,22 +56,28 @@ const SearchStyled = styled.div`
 const InputStyled = styled.input`
   background-color: var(--black);
   border: 1px solid var(--darkwhite);
-  padding: 1rem 4rem 1rem 1.5rem;
+  padding: 1rem 0.5rem 1rem 4rem;
   color: var(--darkwhite);
   border: none;
   outline: none;
   font: 600 1.3rem var(--inter);
   width: 100%;
+  position: relative;
   ::placeholder {
     font: 600 1.3rem var(--inter);
     color: var(--darkwhite);
     text-transform: uppercase;
   }
+  ::-webkit-search-cancel-button {
+    -webkit-appearance: none;
+    height: 2.4rem;
+    width: 2.4rem;
+    border-radius: 50em;
+    background: url(/svg/Cancel.svg) no-repeat;
+    margin-top: 0.3rem;
+    opacity: 1;
+  }
   @media (min-width: 480px) {
     width: 28rem;
   }
-`;
-const IconStyled = styled.div`
-  fill: var(--white);
-  margin: 0.4rem 0 0 -3rem;
 `;
